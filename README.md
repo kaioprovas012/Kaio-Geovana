ecd838a95c629f496d5ed2ea3ae9c7241dffd9bd
kaioprovas012 <kaioprovas012@gmail.com>
2025-07-21 20:59:33 -0300

    Subindo meu projeto

--- README.md
+++
@@ -1,7 +0,0 @@
-# Doritos e Carol
-
-A Pen created on CodePen.
-
-Original URL: [https://codepen.io/Melquisedeque-RA-Melqui/pen/RNWrxbL](https://codepen.io/Melquisedeque-RA-Melqui/pen/RNWrxbL).
-
-# Kaio-Geovana
---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("h1").addEventListener("mouseover", () => {
+  document.querySelector("h1").style.color = "#fff";
+});
+
+document.querySelector("h1").addEventListener("mouseout", () => {
+  document.querySelector("h1").style.color = "#ff69b4";
+});
